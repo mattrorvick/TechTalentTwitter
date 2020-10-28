89b6bb437e19a059a7f500fe3204a0bdb880fab0
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-10-28 12:32:03 -0500

    providing validations for user info implementation

--- src/main/java/com/tts/twitter/model/User.java
+++ src/main/java/com/tts/twitter/model/User.java
@@ -1,6 +1,7 @@
 package com.tts.twitter.model;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -11,44 +12,67 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.persistence.JoinColumn;
 
 import org.hibernate.annotations.CreationTimestamp;
-
-
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class User {
 
-    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
-
     private Long id;
 
+    @Email(message = "Please provide a valid email")
+    @NotEmpty(message = "Please provide an email")
     private String email;
+
+    @Length(min = 3, message = "Your username must have at least 3 characters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
     private String username;
+
+    @Length(min = 5, message = "Your password must have at least 5 characters")
     private String password;
+
+    @NotEmpty(message = "Please provide your first name")
     private String firstName;
+
+    @NotEmpty(message = "Please provide your last name")
     private String lastName;
+
     private int active;
 
     @CreationTimestamp
     private Date createdAt;
 
     @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), 
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_follower", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "follower_id"))
+    private List<User> followers;
 
-    //JPA needs empty constructor 
+    @ManyToMany(mappedBy = "followers")
+    private List<User> following;
+
+    // JPA needs empty constructor
     public User() {
     }
 
-    public User(Long id, String email, String username, String password, String firstName, String lastName, int active,
-            Date createdAt, Set<Role> roles) {
+    public User(Long id,
+            @Email(message = "Please provide a valid email") @NotEmpty(message = "Please provide an email") String email,
+            @Length(min = 3, message = "Your username must have at least 3 characters") @Length(max = 15, message = "Your username cannot have more than 15 characters") @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces") String username,
+            @Length(min = 5, message = "Your password must have at least 5 characters") String password,
+            @NotEmpty(message = "Please provide your first name") String firstName,
+            @NotEmpty(message = "Please provide your last name") String lastName, int active, Date createdAt,
+            Set<Role> roles, List<User> followers, List<User> following) {
         this.id = id;
         this.email = email;
         this.username = username;
@@ -58,6 +82,8 @@ public class User {
         this.active = active;
         this.createdAt = createdAt;
         this.roles = roles;
+        this.followers = followers;
+        this.following = following;
     }
 
     public Long getId() {
@@ -132,6 +158,22 @@ public class User {
         this.roles = roles;
     }
 
+    public List<User> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<User> followers) {
+        this.followers = followers;
+    }
+
+    public List<User> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(List<User> following) {
+        this.following = following;
+    }
+
     @Override
     public String toString() {
         return "User [active=" + active + ", createdAt=" + createdAt + ", email=" + email + ", firstName=" + firstName
@@ -139,8 +181,6 @@ public class User {
                 + ", username=" + username + "]";
     }
 
-    
-    
 
 
 }
